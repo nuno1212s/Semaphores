8d19f7aaa456c3a6fce9e69b01cc757b10bde465
Nuno Neto <nuno.neto.g@gmail.com>
2024-03-22 12:38:22 +0000

    Fixed formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,41 @@
-use std::marker::PhantomData;
-use std::sync::{Arc};
-use std::sync::atomic::{AtomicUsize, Ordering};
 use parking_lot::{Condvar, Mutex};
+use std::marker::PhantomData;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
 
 pub struct RawSemaphore {
     capacity: usize,
     counter: AtomicUsize,
     lock: Mutex<()>,
-    cond: Condvar
+    cond: Condvar,
 }
 
 impl RawSemaphore {
-
     pub fn new(capacity: usize) -> RawSemaphore {
         RawSemaphore {
             capacity,
             counter: AtomicUsize::new(capacity),
             lock: Mutex::new(()),
-            cond: Condvar::new()
+            cond: Condvar::new(),
         }
     }
 
     ///Attempt to acquire a spot in the semaphore
     /// Does not block
     pub fn try_acquire(&self) -> bool {
-
         //Attempt to acquire a spot in the semaphore
         //We use relaxed since all other variables in memory kind of don't matter to us until we have acquired?
         let mut currently_available = self.counter.load(Ordering::Relaxed);
 
         loop {
-
             if currently_available > 0 {
                 //attempt to take the available slot
-                match self.counter.compare_exchange_weak(currently_available, currently_available - 1,
-                                                         Ordering::SeqCst, Ordering::SeqCst) {
+                match self.counter.compare_exchange_weak(
+                    currently_available,
+                    currently_available - 1,
+                    Ordering::SeqCst,
+                    Ordering::SeqCst,
+                ) {
                     Ok(_) => {
                         //Successfully acquired a slot in the semaphore
                         return true;
@@ -63,8 +64,12 @@ impl RawSemaphore {
 
         loop {
             if currently_available > 0 {
-                match self.counter.compare_exchange_weak(currently_available, currently_available - 1,
-                                                         Ordering::SeqCst, Ordering::SeqCst) {
+                match self.counter.compare_exchange_weak(
+                    currently_available,
+                    currently_available - 1,
+                    Ordering::SeqCst,
+                    Ordering::SeqCst,
+                ) {
                     Ok(_) => {
                         return true;
                     }
@@ -82,8 +87,12 @@ impl RawSemaphore {
                 //Will try to lock that mutex so it can wake up the sleeping processes,
                 //This should work 100% of the times
                 if currently_available > 0 {
-                    match self.counter.compare_exchange_weak(currently_available, currently_available - 1,
-                                                             Ordering::SeqCst, Ordering::SeqCst) {
+                    match self.counter.compare_exchange_weak(
+                        currently_available,
+                        currently_available - 1,
+                        Ordering::SeqCst,
+                        Ordering::SeqCst,
+                    ) {
                         Ok(_) => {
                             return true;
                         }
@@ -112,19 +121,18 @@ impl RawSemaphore {
             self.cond.notify_all();
         }
     }
-
 }
 
 struct Semaphore<T> {
     sem: Arc<RawSemaphore>,
-    phantom: PhantomData<T>
+    phantom: PhantomData<T>,
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use std::sync::Barrier;
     use std::time::Duration;
-    use super::*;
 
     #[test]
     pub fn test_single_thread_try_acquire() {
@@ -153,7 +161,6 @@ mod tests {
 
     #[test]
     pub fn test_two_thread_try_acquire() {
-
         let capacity = 5;
 
         let sem = Arc::new(RawSemaphore::new(capacity));
@@ -170,7 +177,6 @@ mod tests {
         let barrier_2 = Arc::clone(&barrier);
 
         let handle = std::thread::spawn(move || {
-
             assert!(!sem2.try_acquire());
             barrier.wait();
 
@@ -201,5 +207,4 @@ mod tests {
 
         handle.join().unwrap();
     }
-
 }
